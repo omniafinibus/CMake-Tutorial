d0e1ef7eaddf4b5fbd6f65b7cca1979e750a4467
Arjan Lemmens <github.mutation602@passmail.net>
2025-05-08 15:18:55 +0200

    finish exercise 3

--- Step1/tutorial.cxx
+++ Step1/tutorial.cxx
@@ -2,14 +2,12 @@
 #include <cmath>
 #include <iostream>
 #include <string>
-
-// TODO 11: Include TutorialConfig.h
+#include "TutorialConfig.h"
 
 int main(int argc, char* argv[])
 {
   if (argc < 2) {
-    // TODO 12: Create a print statement using Tutorial_VERSION_MAJOR
-    //          and Tutorial_VERSION_MINOR
+    std::cout << "Version " << Tutorial_VERSION_MAJOR << "." << Tutorial_VERSION_MINOR << std::endl;
     std::cout << "Usage: " << argv[0] << " number" << std::endl;
     return 1;
   }
